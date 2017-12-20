ee6910a3fa815c7224112894d3ee6732e8c694c4
James <naughtsee@gmail.com>
2017-12-20 03:06:47 -0500

    this is how it ends

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
@@ -31,8 +31,18 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.githubpages']
+extensions = [
+    'sphinx.ext.autodoc', 
+    'sphinx.ext.githubpages',
+    'sphinxcontrib.asyncio'
+]
+
+if os.getenv('READTHEDOCS') == 'True':
+    extensions.append('sphinxcontrib.napoleon')
+else:
+    extensions.append('sphinx.ext.napoleon')
+
+autodoc_member_order = 'bysource'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
