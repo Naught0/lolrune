c1df4b6ca1a2965e93ae308db5796fd271e0a30c
James <naught0@github.com>
2017-12-20 12:49:32 -0500

    learn to spell magic

--- docs/conf.py
+++ docs/conf.py
@@ -19,14 +19,14 @@
 #
 # import os
 import sys
-from unittest.mock import MagickMock
+from unittest.mock import MagicMock
 # sys.path.insert(0, os.path.abspath('.'))
 
 # Attempt to fix aiohttp business
-class Mock(MagickMock):
+class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
-        return MagickMock
+        return MagicMock
 
 MOCK_MODULES = ['aiohttp']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
