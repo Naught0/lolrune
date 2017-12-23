75cae87ae9f951f9ce8fda8ea880c8086fcf2ce4
James <naughtsee@gmail.com>
2017-12-22 23:40:33 -0500

    pip please just package the json

--- setup.py
+++ setup.py
@@ -16,9 +16,6 @@ setup(
     name='lolrune',
     packages=['lolrune'],
     include_package_data=True,
-    package_data={
-        'data': ['rune_links.json']
-    },
     use_scm_version={
         'version_scheme': 'guess-next-dev',
         'local_scheme': 'dirty-tag'
